d797ee4f80e38df1ad380d86b9beb7c3d7555592
ChiakiTakeuchi <takeuchi.chiako1025@gmail.com>
2025-03-26 11:38:21 +0900

    並び替えのクエリ取得＆表示確認ok

--- travel_schedule_project/travelapp/views.py
+++ travel_schedule_project/travelapp/views.py
@@ -206,6 +206,17 @@ class TouristSpotSearchView(ListView):
         if category:
             # カテゴリによるフィルタリング
             queryset = queryset.filter(category=category)
+
+                # 並び順の処理
+        sort_by = self.request.GET.get('sort', '-review_score_average')  # デフォルトは評価がいい順
+        if sort_by == 'review_score_average':
+            queryset = queryset.order_by('-review_score_average')  # 評価がいい順
+        elif sort_by == '-created_at':
+            queryset = queryset.order_by('-created_at')  # 登録が新しい順
+        elif sort_by == 'created_at':
+            queryset = queryset.order_by('created_at')  # 登録が古い順
+
+        
         
         return queryset.distinct()
 
